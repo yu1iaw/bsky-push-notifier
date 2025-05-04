1843aa83f8ab6f09cb4864e33f4def8a34d72795
yu1ia <vasilenyuky@gmail.com>
2025-05-04 08:26:29 +0300

    ✔

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { createClient } from 'redis';
 dotenv.config();
 admin.initializeApp({ credential: admin.credential.cert('./serviceAccountKey.json') });
 
-const redisClient = await createClient({
+const redisClient = createClient({
     socket: {
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT,
@@ -16,32 +16,25 @@ const redisClient = await createClient({
     password: process.env.REDIS_PASSWORD,
 })
     .on("error", (err) => console.error('Redis connection error: ', err))
-    .connect();
 
 
 const agent = new AtpAgent({
     service: 'https://bsky.social',
     persistSession: (evt, session) => {
         console.log('Persisting session', evt);
-        redisClient.set('session', JSON.stringify(session));
     }
 });
 
 const login = async () => {
     try {
-        const session = await redisClient.get('session');
-        if (session) {
-            await agent.resumeSession(JSON.parse(session));
-        } else {
-            console.log('No session found in Redis, logging in...');
-            const { data, headers } = await agent.login({
-                identifier: process.env.BSKY_LOGIN,
-                password: process.env.BSKY_PASSWORD,
-            });
-        }
+        const { data, headers } = await agent.login({
+            identifier: process.env.BSKY_LOGIN,
+            password: process.env.BSKY_PASSWORD,
+        });
+
         return { success: true };
     } catch (error) {
-        return { sussess: false };
+        return { success: false };
     }
 }
 
@@ -60,40 +53,44 @@ const sendFCMPush = async (title) => {
 
     try {
         const response = await admin.messaging().send(message);
-        // console.log('Successfully sent message:', response);
     } catch (error) {
-        console.error('Error sending message:', error);
+        console.error('Error sending push notification:', error);
     }
 }
 
 const checkNotifications = async () => {
-    const response = await agent.listNotifications();
-    const unreadNotifications = response.data.notifications.filter(n => !n.isRead);
-
-    if (unreadNotifications.length) {
-        if (!redisClient.isOpen) {
-            await redisClient.connect();
-        }
-
-        for (const notification of unreadNotifications) {
-            const isProcessed = await redisClient.get(notification.cid);
-            if (isProcessed) continue;
-
-            let message = '';
-            if (notification.reason === 'mention') {
-                message = `📨 New mention from ${notification.author.handle}!`;
-            } else if (notification.reason === 'reply') {
-                message = `💬 New reply from ${notification.author.handle}!`;
+    try {
+        const response = await agent.listNotifications();
+        const unreadNotifications = response.data.notifications.filter(n => !n.isRead);
+    
+        if (unreadNotifications.length) {
+            if (!redisClient.isReady) {
+                await redisClient.connect();
             }
-            if (message) {
-                await sendFCMPush(message);
-                await redisClient.set(notification.cid, 'processed', { EX: 60 * 60 * 24 });
-                await agent.updateSeenNotifications();
+    
+            for (const notification of unreadNotifications) {
+                const isProcessed = await redisClient.get(notification.cid);
+                if (isProcessed) continue;
+    
+                let message = '';
+                if (notification.reason === 'mention') {
+                    message = `📨 New mention from ${notification.author.handle}!`;
+                } else if (notification.reason === 'reply') {
+                    message = `💬 New reply from ${notification.author.handle}!`;
+                }
+                if (message) {
+                    await sendFCMPush(message);
+                    await redisClient.set(notification.cid, 'processed', { EX: 60 * 60 * 36 });
+                    // await agent.updateSeenNotifications();
+                }
             }
         }
-    } 
-    if (redisClient.isOpen) {
-        redisClient.disconnect();
+    } catch (error) {
+        console.error('Error checking notifications:', error);
+    } finally {
+        if (redisClient.isOpen) {
+            redisClient.disconnect();
+        }
     }
 }
 
